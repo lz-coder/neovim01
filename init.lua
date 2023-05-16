e9ff5645376046c8ab9c309300d36a1bbe92f1a1
lz-coder <lzcoder@proton.me>
2023-05-15 22:27:08 -0300

    update

--- init.lua
+++ init.lua
@@ -306,7 +306,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = {'typescript', 'javascript', 'tsx', 'json', 'html', 'css', 'help', 'vim' },
+  ensure_installed = {'typescript', 'javascript', 'tsx', 'json', 'html', 'css', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python', 'html' } },
